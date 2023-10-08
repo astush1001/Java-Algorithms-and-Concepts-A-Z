c8784940f244c5cf6535450cc90e895bf41ccc9a
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2023-10-08 14:04:05 +0530

    fix:comments added to cyclicsort

--- CyclicSort.java
+++ CyclicSort.java
@@ -2,21 +2,26 @@ import java.util.Arrays;
 
 public class CyclicSort {
     public static void main(String[] args) {
-        //int[] nums={5,4,3,2,1,0};     [0 to N] : int correct=arr[i]
-        int[] nums={5,4,3,2,1};         //[1 to N] : int correct=arr[i]-1
+        // Example array: [5, 4, 3, 2, 1]
+        int[] nums = {5, 4, 3, 2, 1};
         sortCyclic(nums);
         System.out.println(Arrays.toString(nums));
     }
-    static void sortCyclic(int[] arr){
-        int i=0;
-        while(i < arr.length){
-            int correct=arr[i]-1;
-            if(arr[i]!=arr[correct]){
-                int t=arr[i];
-                arr[i]=arr[correct];
-                arr[correct]=t;
-            }else{
-                i++;
+
+    static void sortCyclic(int[] arr) {
+        int i = 0;
+        
+        // Continue the process until we have examined all elements in the array.
+        while (i < arr.length) {
+            int correct = arr[i] - 1; // Calculate the correct index for the current element.
+            
+            // If the current element is not in its correct position.
+            if (arr[i] != arr[correct]) {
+                int temp = arr[i];
+                arr[i] = arr[correct];
+                arr[correct] = temp;
+            } else {
+                i++; // Move to the next element as it is already in its correct position.
             }
         }
     }

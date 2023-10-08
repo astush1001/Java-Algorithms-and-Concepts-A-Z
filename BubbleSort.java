10022398673f66ba53dc45a804a1bae80c44fd8b
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2023-10-08 13:59:36 +0530

    fix: comments added to bubblesort

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,24 +2,34 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] num={4,1,3,9,7};
-        bubble(num);
+        int[] num = {4, 1, 3, 9, 7};
+        bubbleSort(num);
         System.out.println(Arrays.toString(num));
     }
 
-    static void bubble(int[] arr){
+    static void bubbleSort(int[] arr) {
         boolean swapped;
+
+        // Outer loop iterates from 0 to n-1 times (n is the array length).
+        // It represents the number of passes required to sort the array.
         for (int i = 0; i < arr.length; i++) {
-            swapped=false;
-            for (int j =1; j <arr.length-i ; j++) {
-                if(arr[j] < arr[j-1]){
-                    int temp=arr[j];
-                    arr[j]=arr[j-1];
-                    arr[j-1]=temp;
-                    swapped=true;
+            swapped = false; // Initialize swapped to false for each pass.
+
+            // Inner loop iterates over the unsorted part of the array.
+            // It compares adjacent elements and swaps them if they are in the wrong order.
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    // Swap arr[j] and arr[j-1]
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    swapped = true; // Set swapped to true if a swap occurred.
                 }
             }
-            if(swapped==false){
+
+            // If no elements were swapped in this pass, the array is already sorted.
+            // We can break out of the loop early to optimize the algorithm.
+            if (!swapped) {
                 break;
             }
         }
